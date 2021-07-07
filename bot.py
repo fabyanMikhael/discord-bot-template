ec969d5e2aa1c70ec2d8159097d0aae2924854f1
fabyanMikhael <fabyanmikhael@yahoo.ca>
2021-07-07 00:58:15 -0400

    added command example

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv() #Loads anything in .env file
 
 TOKEN = os.environ["DISCORD_TOKEN"] #grabs the token after loading it with load_dotenv()
 if TOKEN == "PASTE_TOKEN_HERE":
@@ -14,7 +14,7 @@ if TOKEN == "PASTE_TOKEN_HERE":
 
 import discord
 
-from discord.ext import commands
+from discord.ext import commands #module that contains the Bot class we need
 
 intents = discord.Intents.all() # will require all intents by default, change this to match your bot's intents
 
@@ -32,7 +32,16 @@ async def on_ready(): #the name of the function denotes which event it listens t
 async def on_message(message : discord.Message):
     if message.author == bot.user: #does not attempt to process any command called by the bot itself
         return
-    await bot.process_commands(message=message)
+    await bot.process_commands(message=message) #since we have overwritten the on_message event, we need to call this to process commands
+
+
+#################Commands#################
+'''@bot.command decorator adds this function as a command'''
+@bot.command(aliases=["hello"])  #we can give the aliases key argument a list of alternative command names, such as $hello
+async def Hi(ctx : commands.Context):  #The name of the function is the command, so the `Hi` function is the $Hi command
+    await ctx.reply("hello!") #ctx is the context this command was ran in, .reply() will reply to the user!
+   #^^^ await is needed to call async functions!
+##########################################
 
 bot.run(TOKEN) #runs a bot with the specified [TOKEN], this connects the wrapper to your bot 
 
